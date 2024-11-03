95dd287775b138c0c777bec5a5b5b02619c42f01
EquipoTC <gino.casentini.5@gmail.com>
2024-11-03 20:12:40 -0300

    feat(backend): add room model

---
+++ CasaAPI/Models/RoomModel.cs
@@ -0,0 +1,15 @@
+﻿namespace CasaAPI.Models
+{
+	public class RoomModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public List<LedModel> Leds { get; set; }
+
+		public RoomModel()
+		{
+			Name = "room_name";
+			Leds = new List<LedModel>();
+		}
+	}
+}
