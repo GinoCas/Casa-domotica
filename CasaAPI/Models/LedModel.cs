07a451545e8ecf01bcd5146dfd86954043712154
EquipoTC <gino.casentini.5@gmail.com>
2024-11-03 20:12:19 -0300

    feat(backend): add led model

---
+++ CasaAPI/Models/LedModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CasaAPI.Models
+{
+	public class LedModel
+	{
+		int Id { get; set; }
+		int Pin {  get; set; }
+		bool State { get; set; }
+		int Brightness { get; set; }
+		double Voltage { get; set; } // Volts
+		double Energy { get; set; } // Amperes
+	}
+}
