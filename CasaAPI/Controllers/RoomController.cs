9843d0e23630cffd6863d6e47650cd63cce3d632
EquipoTC <gino.casentini.5@gmail.com>
2024-11-03 20:14:03 -0300

    feat(backend): add room controller

---
+++ CasaAPI/Controllers/RoomController.cs
@@ -0,0 +1,27 @@
+﻿using CasaAPI.Handlers.Room;
+using CasaAPI.Models;
+using CasaAPI.Utils.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasaAPI.Controllers
+{
+	[ApiController]
+	public class RoomController : ControllerBase
+	{
+		private readonly GetHandler getHandler;
+		public RoomController(GetHandler getHandler) 
+		{
+			this.getHandler = getHandler;
+		}
+		[HttpGet("/{roomName}")]
+		public IActionResult GetRoomDevices(string roomName)
+		{
+			Response<RoomModel> response = getHandler.GetRoomByName(roomName);;
+			if (response.cdRes != "OK")
+			{
+				return NotFound(response);
+			}
+			return Ok(response);
+		}
+	}
+}
