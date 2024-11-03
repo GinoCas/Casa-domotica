86d1078a3f33e9725702464108bd2eaeb210cdbb
EquipoTC <gino.casentini.5@gmail.com>
2024-11-03 20:13:49 -0300

    feat(backend): add room get handler

---
+++ CasaAPI/Handlers/Room/GetHandler.cs
@@ -0,0 +1,29 @@
+﻿using CasaAPI.DBContext.Room;
+using CasaAPI.Models;
+using CasaAPI.Utils.Responses;
+
+namespace CasaAPI.Handlers.Room
+{
+	public class GetHandler
+	{
+		private readonly RoomDB context;
+		public GetHandler(RoomDB roomDB)
+		{
+			context = roomDB;
+		}
+		public Response<RoomModel> GetRoomByName(string roomName)
+		{
+			Response<RoomModel> response = new Response<RoomModel>();
+			RoomModel? room = context.GetByName(roomName);
+			if (room == null)
+			{
+				response.cdRes = "ERROR";
+				response.dsRes = "Habitacion inexistente";
+				response.errors.Add(response.dsRes);
+				return response;
+			}
+			response.data.Add(room);
+			return response;
+		}
+	}
+}
