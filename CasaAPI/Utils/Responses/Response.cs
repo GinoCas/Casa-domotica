d3a2732cd1401f9ead067a36a0190a40fc673708
EquipoTC <gino.casentini.5@gmail.com>
2024-11-03 20:12:59 -0300

    feat(backend): add response format class

---
+++ CasaAPI/Utils/Responses/Response.cs
@@ -0,0 +1,19 @@
+﻿namespace CasaAPI.Utils.Responses
+{
+	public class Response<T>
+	{
+		public List<T> data;
+		public string cdRes;
+		public string dsRes;
+		public List<string> errors;
+		public List<string> alerts;
+		public Response()
+		{
+			data = new List<T>();
+			cdRes = "OK";
+			dsRes = "";
+			errors = new List<string>();
+			alerts = new List<string>();
+		}
+	}
+}
