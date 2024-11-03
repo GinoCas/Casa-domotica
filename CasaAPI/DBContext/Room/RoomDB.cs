48c99081ad99fbaf1753292b1a074a319a2d8ea7
EquipoTC <gino.casentini.5@gmail.com>
2024-11-03 20:13:27 -0300

    feat(backend): add room db context

---
+++ CasaAPI/DBContext/Room/RoomDB.cs
@@ -0,0 +1,22 @@
+﻿using CasaAPI.Models;
+
+namespace CasaAPI.DBContext.Room
+{
+	public class RoomDB
+	{
+		private readonly List<RoomModel> rooms;
+		public RoomDB()
+		{
+			rooms = new List<RoomModel>();
+		}
+		public RoomModel? GetByName(string name)
+		{
+			foreach (var room in rooms)
+			{
+				if(room.Name == name) return room;
+			}
+			return null;
+		} 
+
+	}
+}
