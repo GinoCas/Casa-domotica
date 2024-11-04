ed7864c0784d4ad14587dec352797ab7dd0ccd62
EquipoTC <gino.casentini.5@gmail.com>
2024-11-03 22:32:13 -0300

    fix(backend): make variables public in led model

--- CasaAPI/Models/LedModel.cs
+++ CasaAPI/Models/LedModel.cs
@@ -2,12 +2,12 @@
 {
 	public class LedModel
 	{
-		int Id { get; set; }
-		int Pin {  get; set; }
-		bool State { get; set; }
-		int Brightness { get; set; }
-		double Voltage { get; set; } // Volts
-		double Energy { get; set; } // Amperes
+		public int Id { get; set; }
+		public int Pin {  get; set; }
+		public bool State { get; set; }
+		public int Brightness { get; set; }
+		public double Voltage { get; set; } // Volts
+		public double Energy { get; set; } // Amperes
 		public LedModel(int id, int pin, bool state, int brightness, double voltage, double energy)
 		{
 			Id = id;
