dcae0edba0625444a1545dda54a82897711f30ea
EquipoTC <gino.casentini.5@gmail.com>
2024-11-05 01:23:20 -0300

    refactor(app): use .env in post handler

--- CasaApp/Utils/apiHandlers.js
+++ CasaApp/Utils/apiHandlers.js
@@ -1,12 +1,16 @@
+import { Response } from "./responseFormat";
+
 const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 export async function GetHandler(route) {
   const response = await fetch(`${API_URL}/${route}`);
-  return await response.json();
+  const json = await response.json();
+  const { data, cdRes, dsRes, errors, alerts } = json;
+  return new Response(data, cdRes, dsRes, errors, alerts);
 }
 
 export async function PostHandler(route, data) {
-  return await fetch(API_URL + route, {
+  return await fetch(`${API_URL}/${route}`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
