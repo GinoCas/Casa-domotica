0a381298691b287c594e64f672afb2af3da607c2
EquipoTC <gino.casentini.5@gmail.com>
2024-11-05 01:21:53 -0300

    refactor(app): delete get devices from home

--- CasaApp/app/(tabs)/index.jsx
+++ CasaApp/app/(tabs)/index.jsx
@@ -5,14 +5,6 @@ import { GetDeviceList } from "../../lib/deviceController";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [devices, setDevices] = useState([]);
-  useEffect(() => {
-    GetDeviceList().then((devices) => {
-      console.log("x");
-      console.log(devices);
-      setDevices(devices);
-    });
-  }, []);
   const roomName = "living";
   return (
     <Container>
