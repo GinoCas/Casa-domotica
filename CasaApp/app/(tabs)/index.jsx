c73fe406c1943f3ba1c6146bb3f0cfa9269da0d7
EquipoTC <gino.casentini.5@gmail.com>
2024-11-04 23:07:27 -0300

    feat(app): add get devices to home

--- CasaApp/app/(tabs)/index.jsx
+++ CasaApp/app/(tabs)/index.jsx
@@ -1,8 +1,18 @@
 import { Text } from "react-native";
 import { Container } from "../../components/ui/container";
 import { RoomView } from "../../components/room/view";
+import { GetDeviceList } from "../../lib/deviceController";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [devices, setDevices] = useState([]);
+  useEffect(() => {
+    GetDeviceList().then((devices) => {
+      console.log("x");
+      console.log(devices);
+      setDevices(devices);
+    });
+  }, []);
   const roomName = "living";
   return (
     <Container>
