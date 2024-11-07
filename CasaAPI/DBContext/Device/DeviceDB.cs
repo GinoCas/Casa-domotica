4c9a9e5d1cc40df1418e088696028840f39be693
GinoCas <gino.casentini.5@gmail.com>
2024-11-07 16:29:40 -0300

    feat(backend): add device database context

---
+++ CasaAPI/DBContext/Device/DeviceDB.cs
@@ -0,0 +1,30 @@
+﻿using CasaAPI.Interfaces;
+
+namespace CasaAPI.DBContext.Device
+{
+	public class DeviceDB
+	{
+		private readonly List<IDevice> list;
+		public DeviceDB()
+		{
+			list = new List<IDevice>();
+		}
+		public List<IDevice> GetList()
+		{
+			Console.WriteLine(list.Count);
+			return list;
+		}
+		public IDevice? GetById(int id)
+		{
+			if(id >= list.Count)
+			{
+				return null;
+			}
+			return list[id];
+		}
+		public void AddDevice(IDevice device)
+		{
+			list.Add(device);
+		}
+	}
+}
