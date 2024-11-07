94e065877d74bc1610b0fbcf70cc9e9faff809b5
GinoCas <gino.casentini.5@gmail.com>
2024-11-07 16:30:08 -0300

    feaat(backend): add device handlers

---
+++ CasaAPI/Handlers/Device/GetHandler.cs
@@ -0,0 +1,35 @@
+﻿using CasaAPI.DBContext.Device;
+using CasaAPI.Interfaces;
+using CasaAPI.Utils.Responses;
+
+namespace CasaAPI.Handlers.Device
+{
+	public class GetHandler
+	{
+		private readonly DeviceDB dbContext;
+		public GetHandler(DeviceDB dbContext)
+		{
+			this.dbContext = dbContext;
+		}
+		public Response<IDevice> GetById(int id)
+		{
+			Response<IDevice> response = new Response<IDevice>();
+			IDevice? device = dbContext.GetById(id);
+			if(device == null)
+			{
+				response.cdRes = "ERROR";
+				response.dsRes = "El dispositivo es inexistente";
+				response.errors.Add(response.dsRes);
+				return response;
+			}
+			response.data.Add(device);
+			return response;
+		}
+		public Response<IDevice> GetList()
+		{
+			Response<IDevice> response = new Response<IDevice>();
+			response.data = dbContext.GetList();
+			return response;
+		}
+	}
+}
---
+++ CasaAPI/Handlers/Device/PostHandler.cs
@@ -0,0 +1,20 @@
+﻿using CasaAPI.DBContext.Device;
+using CasaAPI.Interfaces;
+using CasaAPI.Utils.Responses;
+
+namespace CasaAPI.Handlers.Device
+{
+	public class PostHandler
+	{
+		private readonly DeviceDB dbContext;
+		public PostHandler(DeviceDB dbContext)
+		{
+			this.dbContext = dbContext;
+		}
+		public Response<IDevice> CreateDevice(IDevice device)
+		{
+			dbContext.AddDevice(device);
+			return new Response<IDevice>();
+		}
+	}
+}
