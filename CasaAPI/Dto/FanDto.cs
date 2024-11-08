5f7547cf7e95cf3d82c7f0a0586a9a44a765dd26
EquipoTC <gino.casentini.5@gmail.com>
2024-11-07 22:19:37 -0300

    feat(backend): add devices dto

---
+++ CasaAPI/Dto/FanDto.cs
@@ -0,0 +1,16 @@
+﻿using CasaAPI.Interfaces;
+
+namespace CasaAPI.Dto
+{
+	public class FanDto : IDeviceDto
+	{
+		public string deviceType { get; set; }
+		public DevicePropertiesDto baseProperties { get; set; }
+		public int speed { get; set; }
+		public FanDto()
+		{
+			deviceType = "Fan";
+			baseProperties = new DevicePropertiesDto();
+		}
+	}
+}
---
+++ CasaAPI/Dto/LedDto.cs
@@ -0,0 +1,16 @@
+﻿using CasaAPI.Interfaces;
+
+namespace CasaAPI.Dto
+{
+    public class LedDto : IDeviceDto
+	{
+		public string deviceType { get; set; }
+		public DevicePropertiesDto baseProperties { get; set; }
+		public int brightness { get; set; }
+		public LedDto()
+		{
+			deviceType = "Led";
+			baseProperties = new DevicePropertiesDto();
+		}
+	}
+}
