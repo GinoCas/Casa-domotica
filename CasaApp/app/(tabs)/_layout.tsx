4a054eb039c039ba1438968b5b6689ed9ff958e8
Agustin Galante <agustin.galante.19@outlook.es>
2024-11-07 22:23:48 -0300

    feat(app): naming tabs screens

--- CasaApp/app/(tabs)/_layout.tsx
+++ CasaApp/app/(tabs)/_layout.tsx
@@ -1,9 +1,27 @@
 import { Tabs } from "expo-router";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
 export default function TabsLayout() {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="index" options={{ title: "Home" }}></Tabs.Screen>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 size={20} name="home" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Automations"
+        options={{
+          title: "Automations",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 size={20} name="robot" color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
