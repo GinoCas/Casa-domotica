baf4231663f256e535627c9208c410b1ab4247f9
EquipoTC <gino.casentini.5@gmail.com>
2024-11-07 22:18:38 -0300

    feat(backend): add device dto interface

---
+++ CasaAPI/Dto/DevicePropertiesDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CasaAPI.Dto
+{
+    public class DevicePropertiesDto
+    {
+        public int id { get; set; }
+        public bool state { get; set; }
+        public double voltage { get; set; }
+        public double amperes { get; set; }
+    }
+}
---
+++ CasaAPI/Interfaces/IDeviceDto.cs
@@ -0,0 +1,10 @@
+﻿using CasaAPI.Dto;
+
+namespace CasaAPI.Interfaces
+{
+    public interface IDeviceDto
+    {
+        public string deviceType { get; set; }
+		public DevicePropertiesDto baseProperties { get; set; }
+    }
+}
