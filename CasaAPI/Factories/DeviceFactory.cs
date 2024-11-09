5836b26a2aee5a255862d7905fb8173fbbbb1319
GinoCas <gino.casentini.5@gmail.com>
2024-11-09 11:46:20 -0300

    refactor(app): delete amperes, volts from IDevice

--- CasaAPI/Factories/DeviceFactory.cs
+++ CasaAPI/Factories/DeviceFactory.cs
@@ -8,12 +8,9 @@ namespace CasaAPI.Factories
 	public class DeviceFactory
 	{
 		public readonly Dictionary<string, Func<dynamic, IDevice>> factory;
-		private static IDevice MapBaseProperties<T>(dynamic device, IDevice model)
 		{
 			model.id = (int)device.baseProperties.id;
 			model.state = (bool)device.baseProperties.state;
-			model.voltage = (double)device.baseProperties.voltage;
-			model.amperes = (double)device.baseProperties.amperes;
 			return model;
 		}
 		public DeviceFactory()
@@ -22,7 +19,6 @@ namespace CasaAPI.Factories
 			{
 				{ "Led", device =>
 					{
-						LedModel led = MapBaseProperties<DevicePropertiesDto>(device, new LedModel());
 						led.brightness = (int)device.brightness;
 						return led;
 					}
--- CasaAPI/Interfaces/IDevice.cs
+++ CasaAPI/Interfaces/IDevice.cs
@@ -6,7 +6,5 @@
         public int id { get; set; }
         public int pin { get; set; }
         public bool state { get; set; }
-        public double voltage { get; set; } // Volts
-        public double amperes { get; set; } // Amperes
     }
 }
