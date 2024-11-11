f849cf6fcb68de722b4f54bbe4c64eee4c1f0dea
Agustin Galante <agustin.galante.19@outlook.es>
2024-11-11 12:05:29 -0300

    feat(app): automations switch to turn on/off

--- CasaApp/components/automations/automation-card.tsx
+++ CasaApp/components/automations/automation-card.tsx
@@ -1,24 +1,38 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import GlobalStyles from "@/Utils/globalStyles";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { Switch } from "../ui/switch";
 
 export default function AutomationCard({ automation }: { automation?: any }) {
+  const [isEnabled, setIsEnabled] = useState(false);
+
   return (
-    <View style={styles.cardContainer}>
+    <View
+      style={[
+        styles.cardContainer,
+        { backgroundColor: isEnabled ? "#fff" : "#e1e1e1" },
+      ]}
+    >
       <View>
         <Text style={styles.title}>{automation.name}</Text>
         <Text style={styles.subtitle}>{automation.description}</Text>
       </View>
-      <TouchableOpacity>
-        <Text>
-          <FontAwesome5
-            name="chevron-right"
-            size={20}
-            color={GlobalStyles.enabledColor}
-          />
-        </Text>
-      </TouchableOpacity>
+      <View style={styles.actionsContainer}>
+        <Switch
+          toggleEnabled={() => setIsEnabled(!isEnabled)}
+          isEnabled={isEnabled}
+        />
+        <TouchableOpacity>
+          <Text>
+            <FontAwesome5
+              name="chevron-right"
+              size={20}
+              color={GlobalStyles.enabledColor}
+            />
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -41,5 +55,10 @@ const styles = StyleSheet.create({
   subtitle: {
     fontSize: 14,
     color: "#a6a6a6",
+  },
+  actionsContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 16,
   },
 });
