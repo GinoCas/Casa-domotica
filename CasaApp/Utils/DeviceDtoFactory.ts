6ba654ca51c169c857d2d7a969f7760d970d0d3e
EquipoTC <gino.casentini.5@gmail.com>
2024-11-13 00:52:25 -0300

    feat(app): add device dto factory

---
+++ CasaApp/Utils/DeviceDtoFactory.ts
@@ -0,0 +1,20 @@
+import Device from "@/types/Device";
+
+export function createDeviceDto(device: Device) {
+  const dto = [
+    device.deviceType,
+    device.baseProperties.id,
+    device.baseProperties.state,
+  ];
+  const dtoFactory: { [key: string]: (device: Device) => void } = {
+    Led: (device) => {
+      dto.push(device.brightness);
+    },
+    Motor: (device) => {
+      dto.push(device.speed);
+    },
+  };
+  dtoFactory[device.deviceType]?.(device);
+
+  return dto;
+}
