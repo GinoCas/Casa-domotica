e14060a8e5e67ea7383cdef19c4a1f5366ff8d06
EquipoTC <gino.casentini.5@gmail.com>
2024-11-15 03:07:24 -0300

    refactor(app): delete unnecesary dependencies
    Delete unnecesary dependencies from automations controller

--- CasaApp/lib/automationController.ts
+++ CasaApp/lib/automationController.ts
@@ -1,6 +1,5 @@
 import AutomationsData from "@/stores/automations.json";
-import { Automation } from "@/types/automation";
-import getTimeString from "@/Utils/getTimeString";
+import { Automation } from "@/types/Automation";
 import { GetDeviceById, UpdateDevice } from "./deviceController";
 
 export function GetAutomationById(id: number) {
