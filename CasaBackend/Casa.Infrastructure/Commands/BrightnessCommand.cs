e559f3397d4e9a327e49c57f016c68d0ba3f8529
Gino <gino.casentini.5@gmail.com>
2025-07-01 17:38:50 -0300

    feat(backend): add brightness command

---
+++ CasaBackend/Casa.Infrastructure/Commands/BrightnessCommand.cs
@@ -0,0 +1,25 @@
+﻿using CasaBackend.Casa.Core.Interfaces.Command;
+using CasaBackend.Casa.Core.Models;
+
+namespace CasaBackend.Casa.Infrastructure.Commands
+{
+    public class BrightnessCommand : ICommand
+    {
+        public string CommandName => "SetBrightness";
+        private readonly Device Device;
+        private readonly int Brightness;
+        public BrightnessCommand(Device device, int brightness)
+        {
+            Device = device;
+            Brightness = brightness;
+        }
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+        public bool CanExecute()
+        {
+            return true;
+        }
+    }
+}
