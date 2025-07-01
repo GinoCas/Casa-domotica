54cf59bec384ab0edd60f9286cbce3a78ef0a17d
Gino <gino.casentini.5@gmail.com>
2025-07-01 17:39:32 -0300

    feat(backend): add repository interface

---
+++ CasaBackend/Casa.Core/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace CasaBackend.Casa.Core.Interfaces.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task<CoreResult<T>> GetByIdAsync(int id);
+        Task<CoreResult<IEnumerable<T>>> GetAllAsync();
+        Task<CoreResult<T>> CreateAsync(T room);
+        Task<CoreResult<T>> UpdateAsync(T room);
+        Task<CoreResult<bool>> DeleteAsync(int id);
+    }
+}
