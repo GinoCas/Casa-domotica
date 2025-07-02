943536cb3e1880e40b791576aa80e02777a58efc
Gino <gino.casentini.5@gmail.com>
2025-07-02 07:24:30 -0300

    feat(backend): add IDimmable

---
+++ CasaBackend/Casa.Core/Interfaces/Device/IDimmable.cs
@@ -0,0 +1,8 @@
+﻿namespace CasaBackend.Casa.Core.Interfaces.Device
+{
+    public interface IDimmable
+    {
+        int Brightness { get; set;  }
+        int[] Limits { get; }
+    }
+}
--- CasaBackend/Casa.Core/Models/LedDevice.cs
+++ CasaBackend/Casa.Core/Models/LedDevice.cs
@@ -1,28 +1,12 @@
+using CasaBackend.Casa.Core.Interfaces.Device;
 using CasaBackend.Casa.Core.Models.ValueObjects;
 
 namespace CasaBackend.Casa.Core.Models
 {
-    public class LedDevice : Device
+    public class LedDevice : Device, IDimmable
     {
         public override DeviceType DeviceType => DeviceType.Led;
-        public int Brightness { get; private set; } = 255;
-        public bool IsDimmable { get; set; } = true;
-        
-        public override void TurnOn()
-        {
-            base.TurnOn();
-            if (Brightness == 0)
-                Brightness = 255;
-        }
-        
-        public override void TurnOff()
-        {
-            base.TurnOff();
-        }
-        
-        public void SetBrightness(int brightness)
-        {
-            Brightness = brightness;
-        }
+        public int Brightness { get; set; } = 255;
+        public int[] Limits => [0, 255];
     }
 }
