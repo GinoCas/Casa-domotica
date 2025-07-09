81f5ebc36fd2f129052ddea9900b8c3ca5011709
Gino <gino.casentini.5@gmail.com>
2025-07-09 01:14:41 -0300

    feat(backend): add injections

--- CasaBackend/Program.cs
+++ CasaBackend/Program.cs
@@ -1,8 +1,43 @@
+using Casa.Core.Interfaces.Repositories;
+using CasaBackend.Casa.Core.Entities;
+using CasaBackend.Casa.Core.Entities.Capabilities;
+using CasaBackend.Casa.Core.Interfaces.Command;
+using CasaBackend.Casa.Core.Interfaces.Repositories;
+using CasaBackend.Casa.Core.Interfaces.Services;
+using CasaBackend.Casa.Infrastructure.Commands;
+using CasaBackend.Casa.Infrastructure.Factories;
+using CasaBackend.Casa.Infrastructure.Mapper;
+using CasaBackend.Casa.Infrastructure.Repositories;
+using CasaBackend.Casa.InterfaceAdapter;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CasaDB"));
+});
+//Repositorios
+builder.Services.AddScoped<IRepository<DeviceEntity>, DeviceRepository>();
+builder.Services.AddScoped<ICapabilityRepository<DimmableEntity>, DimmableRepository>();
 
+//Handlers
+builder.Services.AddScoped<ICommandHandler, BrightnessCommand>();
+
+//Fabricas
+builder.Services.AddScoped<CommandFactory>();
+builder.Services.AddScoped<DeviceFactory>();
+
+//Servicios
+builder.Services.AddScoped<ICommandService, CommandService>();
+
+
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.AddProfile<MappingProfile>();
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
