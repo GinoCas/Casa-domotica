4e41c5e35d91af11539b31e990ad09dda26302ce
Gino <gino.casentini.5@gmail.com>
2025-07-09 01:12:02 -0300

    feat(backend): add command factory

---
+++ CasaBackend/Casa.Infrastructure/Factories/CommandFactory.cs
@@ -0,0 +1,18 @@
+﻿using CasaBackend.Casa.Core.Interfaces.Command;
+
+namespace CasaBackend.Casa.Infrastructure.Factories
+{
+    public class CommandFactory
+    {
+        private readonly IEnumerable<ICommandHandler> _commands;
+        public CommandFactory(IEnumerable<ICommandHandler> commands)
+        {
+            _commands = commands;
+        }
+        public ICommandHandler GetCommand(string commandName)
+        {
+            var cmd = _commands.FirstOrDefault(h => h.CommandName == commandName);
+            return cmd ?? throw new NotSupportedException($"Comando '{commandName}' no soportado.");
+        }
+    }
+}
