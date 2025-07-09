b8c558ab5b1a85f872c4d6d3b9cb2b02373839b5
Gino <gino.casentini.5@gmail.com>
2025-07-09 01:06:00 -0300

    feat(backend): add command dto

---
+++ CasaBackend/Casa.API/DTOs/CommandDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace CasaBackend.Casa.API.DTOs
+{
+    public class CommandDto
+    {
+        public int DeviceId { get; set; }
+        public string CommandName { get; set; }
+        public Dictionary<string, JsonElement> Parameters { get; set; }
+
+    }
+}
--- CasaBackend/Casa.API/DTOs/DeviceDto.cs
+++ CasaBackend/Casa.API/DTOs/DeviceDto.cs
@@ -5,5 +5,6 @@
 		public int Id { get; set; }
 		public int Pin { get; set; }
 		public bool State { get; set; }
+		public int DeviceType { get; set; }
 	}
 }
