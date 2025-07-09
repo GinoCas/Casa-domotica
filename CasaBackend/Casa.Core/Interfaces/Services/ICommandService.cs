5e9cba0d489ffd895d1693141cf93555928f1550
Gino <gino.casentini.5@gmail.com>
2025-07-09 01:13:18 -0300

    feat(backend): add command service

---
+++ CasaBackend/Casa.Core/Interfaces/Services/ICommandService.cs
@@ -0,0 +1,9 @@
+﻿using CasaBackend.Casa.API.DTOs;
+
+namespace CasaBackend.Casa.Core.Interfaces.Services
+{
+    public interface ICommandService
+    {
+        Task<CoreResult<bool>> ExecuteAsync(CommandDto dto);
+    }
+}
---
+++ CasaBackend/Casa.Infrastructure/Repositories/CommandService.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using CasaBackend.Casa.API.DTOs;
+using CasaBackend.Casa.Core;
+using CasaBackend.Casa.Core.Entities;
+using CasaBackend.Casa.Core.Interfaces.Repositories;
+using CasaBackend.Casa.Core.Interfaces.Services;
+using CasaBackend.Casa.Infrastructure.Factories;
+
+namespace CasaBackend.Casa.Infrastructure.Repositories
+{
+    public class CommandService : ICommandService
+    {
+        private readonly IMapper _mapper;
+        private readonly CommandFactory _commandFactory;
+        private readonly IRepository<DeviceEntity> _deviceRepository;
+
+        public CommandService(IMapper mapper, CommandFactory commandFactory, IRepository<DeviceEntity> deviceRepository)
+        {
+            _mapper = mapper;
+            _commandFactory = commandFactory;
+            _deviceRepository = deviceRepository;
+        }
+
+        public async Task<CoreResult<bool>> ExecuteAsync(CommandDto dto)
+        {
+            var command = _mapper.Map<CommandEntity>(dto);
+            var deviceModel = await _deviceRepository.GetByIdAsync(dto.DeviceId);
+            command.Device = _mapper.Map<DeviceEntity>(deviceModel);
+            var handler = _commandFactory.GetCommand(command.CommandName);
+            await handler.HandleAsync(command);
+            return new CoreResult<bool>();
+        }
+    }
+}
