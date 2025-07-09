eaa915d7086ff25fde55183ab5a7476972f5398b
Gino <gino.casentini.5@gmail.com>
2025-07-09 01:04:52 -0300

    feat(backend): add AppDbContext

---
+++ CasaBackend/Casa.InterfaceAdapter/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CasaBackend.Casa.InterfaceAdapter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CasaBackend.Casa.InterfaceAdapter
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<DeviceModel> Devices { get; set; }
+        public DbSet<DimmableModel> Dimmables { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DeviceModel>().ToTable("device");
+            modelBuilder.Entity<DimmableModel>().ToTable("dimmable");
+        }
+    }
+}
