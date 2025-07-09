4dd034b97f46b62108bf88ebc40fac22740f9695
Gino <gino.casentini.5@gmail.com>
2025-07-09 01:07:06 -0300

    feat(backend): add command & dimmable entity

---
+++ CasaBackend/Casa.Core/Entities/Capabilities/DimmableEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace CasaBackend.Casa.Core.Entities.Capabilities
+{
+    public class DimmableEntity
+    {
+        public int Id { get; set; }
+        public int Brightness { get; set; }
+        public int[] Limits => [0, 255];
+    }
+}
---
+++ CasaBackend/Casa.Core/Entities/CommandEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json;
+
+namespace CasaBackend.Casa.Core.Entities
+{
+    public class CommandEntity
+    {
+        public DeviceEntity Device { get; set; }
+        public string CommandName { get; set; } = string.Empty;
+        public Dictionary<string, JsonElement> Parameters { get; set; } = [];
+    }
+}
