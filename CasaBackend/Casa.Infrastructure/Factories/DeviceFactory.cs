ed88cf9808fc1ef78e625809cae9f5a35a278493
Gino <gino.casentini.5@gmail.com>
2025-07-09 01:14:18 -0300

    feat(backend): add device factory

---
+++ CasaBackend/Casa.Infrastructure/Factories/DeviceFactory.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using CasaBackend.Casa.Core.Entities;
+using CasaBackend.Casa.Core.Entities.Capabilities;
+using CasaBackend.Casa.Core.Entities.ValueObjects;
+using CasaBackend.Casa.Core.Interfaces.Repositories;
+using CasaBackend.Casa.InterfaceAdapter.Models;
+
+namespace CasaBackend.Casa.Infrastructure.Factories
+{
+    public class DeviceFactory
+    {
+        private readonly ICapabilityRepository<DimmableEntity> _dimmableRepo;
+        private readonly IMapper _mapper;
+
+        public DeviceFactory(ICapabilityRepository<DimmableEntity> dimmableRepo, IMapper mapper)
+        {
+            _dimmableRepo = dimmableRepo;
+            _mapper = mapper;
+        }
+
+        public async Task<DeviceEntity> CreateAsync(DeviceModel model)
+        {
+            var type = Enum.Parse<DeviceType>(model.DeviceType);
+
+            switch (type)
+            {
+                case DeviceType.Led:
+                    var dimm = await _dimmableRepo.GetByDeviceIdAsync(model.Id);
+                    return new LedEntity(model, _mapper.Map<DimmableEntity>(dimm));
+                default:
+                    throw new NotSupportedException($"Device type {type} not supported.");
+            }
+        }
+    }
+}
