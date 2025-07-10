617ed76b9f51bde9270ec0a06d36d347cc2af6e4
Gino <gino.casentini.5@gmail.com>
2025-07-09 21:01:37 -0300

    feat(backend): add get device use case

---
+++ CasaBackend/Casa.Application/UseCases/GetDeviceUseCase.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using CasaBackend.Casa.Application.Interfaces.Factory;
+using CasaBackend.Casa.Application.Interfaces.Presenter;
+using CasaBackend.Casa.Application.Interfaces.Repositories;
+
+namespace CasaBackend.Casa.Application.UseCases
+{
+    public class GetDeviceUseCase<TEntity, TView>
+        (
+        IRepository<TEntity> repository, 
+        IPresenter<TEntity, TView> presenter
+        )
+    {
+        private readonly IRepository<TEntity> _repository = repository;
+        private readonly IPresenter<TEntity, TView> _presenter = presenter;
+        public async Task<TView> ExecuteAsync(int id)
+        {
+            var entity = await _repository.GetByIdAsync(id);
+            return _presenter.Present(entity);
+        }
+        public async Task<IEnumerable<TView>> ExecuteAsync()
+        {
+            var devices = await _repository.GetAllAsync();
+            IEnumerable<TView> views = [];
+            return devices.Select(_presenter.Present).ToList();
+        }
+    }
+}
