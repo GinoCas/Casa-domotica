9f79002f55cdd660829d6fbb50431621b0f237c5
Gino <gino.casentini.5@gmail.com>
2025-07-12 01:53:32 -0300

    feat(backend): add capability provider interface

---
+++ CasaBackend/Casa.Application/Interfaces/Providers/ICapabilityProvider.cs
@@ -0,0 +1,10 @@
+﻿using CasaBackend.Casa.InterfaceAdapter.Models.Capabilities;
+
+namespace CasaBackend.Casa.Application.Interfaces.Providers
+{
+    public interface ICapabilityProvider
+    {
+        Task<ICapabilityModel?> GetByDeviceIdAsync(int deviceId);
+        Task<Dictionary<int, ICapabilityModel>> GetAllAsync();
+    }
+}
