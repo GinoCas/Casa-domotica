a1b61b93381aeb51225d219f4f5147595b7d486b
Gino <gino.casentini.5@gmail.com>
2025-07-12 01:53:54 -0300

    feat(backend): add capability service

---
+++ CasaBackend/Casa.Infrastructure/Services/CapabilityService.cs
@@ -0,0 +1,35 @@
+﻿using CasaBackend.Casa.Application.Interfaces.Providers;
+using CasaBackend.Casa.InterfaceAdapter.Models.Capabilities;
+
+namespace CasaBackend.Casa.Infrastructure.Services
+{
+    public class CapabilityService(IEnumerable<ICapabilityProvider> providers)
+    {
+        private readonly IEnumerable<ICapabilityProvider> _providers = providers;
+
+        public async Task<IEnumerable<ICapabilityModel>> GetCapabilitiesForDeviceAsync(int deviceId)
+        {
+            var capabilities = new List<ICapabilityModel>();
+            foreach (var provider in _providers)
+            {
+                var capability = await provider.GetByDeviceIdAsync(deviceId);
+                if (capability != null) capabilities.Add(capability);
+            }
+            return capabilities;
+        }
+        public async Task<Dictionary<int, List<ICapabilityModel>>> GetAllCapabilitiesAsync()
+        {
+            var result = new Dictionary<int, List<ICapabilityModel>>();
+            foreach (var provider in _providers)
+            {
+                var capabilities = await provider.GetAllAsync();
+                foreach (var (deviceId, capability) in capabilities)
+                {
+                    if (!result.ContainsKey(deviceId)) result[deviceId] = [];
+                    result[deviceId].Add(capability);
+                }
+            }
+            return result;
+        }
+    }
+}
