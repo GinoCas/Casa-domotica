ce9cde7cf7f20498cfad5d2f30b2d8dea1b3e84f
Gino <gino.casentini.5@gmail.com>
2025-07-12 01:50:49 -0300

    feat(backend): add device context dto

---
+++ CasaBackend/Casa.InterfaceAdapter/DTOs/DeviceContextDto.cs
@@ -0,0 +1,11 @@
+﻿using CasaBackend.Casa.InterfaceAdapter.Models;
+using CasaBackend.Casa.InterfaceAdapter.Models.Capabilities;
+
+namespace CasaBackend.Casa.InterfaceAdapter.DTOs
+{
+    public class DeviceContextDto
+    {
+        public DeviceModel DeviceModel { get; set; }
+        public IEnumerable<ICapabilityModel> Capabilities = [];
+    }
+}
