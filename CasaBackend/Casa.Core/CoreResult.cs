88961c1fbfe3cd4a4f216f26f36e61ecf6e01041
Gino <gino.casentini.5@gmail.com>
2025-07-13 03:25:24 -0300

    feat(backend): add CoreResult delete ApiResponse

---
+++ CasaBackend/Casa.Core/CoreResult.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace CasaBackend.Casa.Core
+{
+    public class CoreResult<T>(T data, bool success, ICollection<string> errors)
+    {
+        public T Data { get; } = data;
+        public bool IsSuccess { get; } = success;
+        public ICollection<string> Errors { get; } = errors;
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+        public static CoreResult<T> Success(T data) => new(data, true, []);
+        public static CoreResult<T> Failure(ICollection<string> errors) => new(default, false, errors);
+    }
+}
