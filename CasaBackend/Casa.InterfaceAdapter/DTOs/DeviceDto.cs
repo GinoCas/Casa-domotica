1bab591098b26b8e361bdc65076017bda47fa241
Gino <gino.casentini.5@gmail.com>
2025-07-13 03:26:26 -0300

    refactor(backend): change DeviceType type in dto

--- CasaBackend/Casa.InterfaceAdapter/DTOs/DeviceDto.cs
+++ CasaBackend/Casa.InterfaceAdapter/DTOs/DeviceDto.cs
@@ -3,8 +3,7 @@
 	public class DeviceDto
 	{
 		public int Id { get; set; }
-		public int Pin { get; set; }
 		public bool State { get; set; }
-		public int DeviceType { get; set; }
+		public string DeviceType { get; set; } = string.Empty;
 	}
 }
