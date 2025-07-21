db9497cb050910e949536612b6e71bb3faf4b1af
Gino <gino.casentini.5@gmail.com>
2025-07-21 06:54:50 -0300

    refactor(app): use result method in api handler

--- CasaApp/Utils/apiHandlers.ts
+++ CasaApp/Utils/apiHandlers.ts
@@ -1,31 +1,41 @@
-import ApiResponse from "@/types/ApiResponse";
-
 const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
-export async function GetHandler<T>(route: string): Promise<ApiResponse<T>> {
-  const response = await fetch(`${API_URL}/${route}`);
-  const json = await response.json();
-  return json;
+const handleResponseError = (err: any): Result<any> =>
+{
+  console.log("Error: " + err);
+  return Result.failure([err]);
 }
 
-export async function PostHandler(route: string, data: any) {
-  return await fetch(`${API_URL}/${route}`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  });
+const handleResponseValidation = async (response: Response): Promise<void> =>
+{
+  if(response.ok) { return }
+  var errText = await response.text();
+  throw new Error(errText);
 }
 
-export async function PutHandler<T>(
-  route: string,
-  data: any,
-): Promise<ApiResponse<T>> {
-  const request = await fetch(`${API_URL}/${route}`, {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  });
-  const response = await request.json();
+export async function GetHandler<T>(route: string): Promise<Result<T>> {
+  try{
+    const response = await fetch(`${API_URL}/${route}`);
+    await handleResponseValidation(response);
+    const json = await response.json();
+    return json as Result<T>;
+  }catch(err: any){
+    return handleResponseError(err);
+  }
+}
 
-  return response;
+export async function PostHandler<T>(route: string, data: any): Promise<Result<T>> {
+  try
+  {
+      const response = await fetch(`${API_URL}/${route}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    await handleResponseValidation(response);
+    const json = await response.json();
+    return json as Result<T>
+  } catch(err){
+    return handleResponseError(err);
+  }
 }
