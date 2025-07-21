176756ac07c16aabef56bd6bc03655672afc15b7
Gino <gino.casentini.5@gmail.com>
2025-07-21 06:54:28 -0300

    feat(app): add result class

---
+++ CasaApp/types/Response.ts
@@ -0,0 +1,20 @@
+interface IResult<T> {
+  data: T;
+  isSuccess: boolean;
+  errors: string[];
+}
+
+class Result<T> implements IResult<T> {
+    constructor(
+        public data: T,
+        public isSuccess: boolean,
+        public errors: string[],
+    ) {}
+  static success<T>(data: T): Result<T> {
+    return new Result(data, true, []);
+  }
+
+  static failure<T>(errors: string[]): Result<T> {
+    return new Result<T>(null as any, false, errors);
+  }
+}
