f34195fdbec1556971b4a1c648385f21a0b78941
Gino <gino.casentini.5@gmail.com>
2025-07-28 16:43:05 -0300

    feat(backend): add room entity

---
+++ CasaBackend/Casa.Core/Entities/RoomEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace CasaBackend.Casa.Core.Entities
+{
+    public class RoomEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public IEnumerable<int> DevicesId { get; set; } = [];
+    }
+}
