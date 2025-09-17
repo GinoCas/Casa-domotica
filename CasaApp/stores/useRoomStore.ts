7ef9bfb61bd1db2ca22e83a3d1bdd17657ae3b99
Gino <gino.casentini.5@gmail.com>
2025-09-17 11:45:09 -0300

    wip(app): room store

--- CasaApp/stores/useRoomStore.ts
+++ CasaApp/stores/useRoomStore.ts
@@ -1,30 +1,37 @@
-import { Device } from "@/types/Device";
+import { Room } from "@/src/core/entities/Room";
+import { Result } from "@/src/shared/Result";
 import { create } from "zustand";
 
 interface RoomState {
-  roomName: string;
-  roomDevices: Device[];
-  handleLoadRoomDevices: (newDevices: Device[]) => void;
-  changeCurrentRoom: (newRoom: string) => void;
+  rooms: Room[];
+  currentRoom: Room | undefined;
   isLoadingRooms: boolean;
-  isLoadingRoomDevices: boolean;
+  handleLoadRooms: (rooms: Room[]) => void;
+  changeCurrentRoom: (newRoom: Room) => void;
   changeLoadingRooms: (newState: boolean) => void;
-  changeLoadingRoomDevices: (newState: boolean) => void;
+  getRoomByName: (roomName: string) => Result<Room>;
 }
 
-const useRoomStore = create<RoomState>()((set) => ({
-  roomDevices: [],
-  roomName: "",
-  handleLoadRoomDevices: (newDevices) =>
-    set((state) => ({ ...state, roomDevices: newDevices })),
-  changeCurrentRoom: (newRoom) =>
-    set((state) => ({ ...state, roomName: newRoom })),
-  isLoadingRoomDevices: false,
+const useRoomStore = create<RoomState>()((set, get) => ({
+  rooms: [],
+  currentRoom: undefined,
+  changeCurrentRoom: (newRoom: Room) =>
+    set((state) => ({ ...state, currentRoom: newRoom })),
   isLoadingRooms: false,
-  changeLoadingRoomDevices: (newState) =>
-    set((state) => ({ ...state, isLoadingRoomDevices: newState })),
   changeLoadingRooms: (newState) =>
     set((state) => ({ ...state, isLoadingRooms: newState })),
+  handleLoadRooms: (newRooms: Room[]) => {
+    set((state) => ({ ...state, rooms: newRooms }));
+  },
+  getRoomByName: (name: string) => {
+    const room = get().rooms.find((item) => item.name === name);
+    if (room === undefined) {
+      return Result.failure([
+        "La habitacion con el nombre: " + name + " no fue encontrada",
+      ]);
+    }
+    return Result.success(room);
+  },
 }));
 
 export default useRoomStore;
