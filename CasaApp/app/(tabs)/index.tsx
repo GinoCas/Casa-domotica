af3068ca404c94585bc5adc6f21370d65f2c67f6
GinoCas <gino.casentini.5@gmail.com>
2025-10-23 09:41:14 -0300

    feat(app): add name to app

--- CasaApp/app/(tabs)/index.tsx
+++ CasaApp/app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import { Text } from "react-native";
 import { RoomView } from "@/components/room/view";
 import { Container } from "@/components/ui/container";
 import useDevices from "@/hooks/useDevices";
+import GlobalStyles from "@/Utils/globalStyles";
 
 export default function Home() {
   const { roomDevicesMemo, isLoadingDevices, unassignedDevices } = useDevices();
@@ -13,10 +14,14 @@ export default function Home() {
           fontSize: 26,
           fontWeight: 600,
           textAlign: "center",
-          marginBottom: 20,
+          marginBottom: 5,
         }}
       >
-        ¡Bienvenido!
+        Encedé<Text style={{ color: GlobalStyles.disabledColor }}>.</Text>
+        <Text style={{ color: "#f1c40f" }}>me</Text>
+      </Text>
+      <Text style={{ textAlign: "center", marginVertical: 4 }}>
+        Encendé tu casa, estés donde estés.
       </Text>
       <RoomView
         devices={roomDevicesMemo}
